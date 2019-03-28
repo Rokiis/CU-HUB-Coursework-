741cceb4f486ab7d7d0761047e0c0d1ef94f2966
Rokas Navickas (navickar) <navickar@coventry.ac.uk>
2019-03-28 04:49:29 +0000

    changed innitial map path

--- src/components/layout/Events.js
+++ src/components/layout/Events.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
-import MapContainer from "../Containers/MapContainer";
+import Map from "../Containers/Map";
+import MapContainer from "../Containers/MapContainer"
 import "./CSS/events.css";
+
 // ────────────────────────────────────────────────────────────────────────────────
 
 class Events extends Component {
   render() {
     return (
       <div className=" h-100">
-        <MapContainer />
+        <Map/>
         <div className="split">
           <div className="row1 height50">
 
           </div>
+          
           <div className="row2 height50">
 
           </div>
